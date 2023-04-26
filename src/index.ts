3d5ceaf05439fc423f73ac66c98b14c66caf4a04
Erisa A <erisa@erisa.uk>
2023-04-26 01:42:11 +0000

    Revert "Increase cacheTtl to 1 day"
    This reverts commit 038f80db03436aded04cf27d190151eb3c74a8f4.

--- src/index.ts
+++ src/index.ts
@@ -120,7 +120,7 @@ app.head('*', handleGetHead)
 // handle both GET and HEAD
 async function handleGetHead(c: Context) {
 	// actual logic goes here
-	const urlResult = await c.env.KV.get(c.get('key'), { cacheTtl: 86400 })
+	const urlResult = await c.env.KV.get(c.get('key'))
 
 	if (urlResult == null) {
 		return c.json(
@@ -144,7 +144,7 @@ async function handleGetHead(c: Context) {
 
 // delete specific key
 app.delete('*', async (c) => {
-	const urlResult = await c.env.KV.get(c.get('key'), { cacheTtl: 86400 })
+	const urlResult = await c.env.KV.get(c.get('key'))
 
 	if (urlResult == null) {
 		return c.json(
